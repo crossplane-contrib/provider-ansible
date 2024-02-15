111b324de6004c736f8e0142cc9b2ddc174afe24
Dasha Komsa <komsa.darya@gmail.com>
2024-02-15 21:46:45 +0000

    unit test for extractFailureReason
    Signed-off-by: Dasha Komsa <komsa.darya@gmail.com>

--- internal/ansible/ansible_test.go
+++ internal/ansible/ansible_test.go
@@ -18,6 +18,7 @@ package ansible
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"os"
 	"os/exec"
@@ -228,6 +229,82 @@ func TestRun(t *testing.T) {
 
 			if string(out) != tc.expectedOutput {
 				t.Errorf("Unexpected output in the command buffer %q, want %q", string(out), tc.expectedOutput)
+			}
+		})
+	}
+}
+
+func TestExtractFailureReason(t *testing.T) {
+	playbookStartEvt := `
+	{
+		"uuid": "63a52ed5-a403-4512-a430-c95f62fa3424",
+		"event": "playbook_on_start",
+		"event_data": {
+			"playbook": "playbook.yml"
+		}
+	}
+	`
+
+	runnerFailedEvt := `
+	{
+		"uuid": "7097758b-1109-4fd9-af59-f545633794dd",
+		"event": "runner_on_failed",
+		"event_data": {
+			"play": "test",
+			"task": "file",
+			"host": "testhost",
+			"res": {"msg": "fake error"}
+		}
+	}
+	`
+
+	runnerUnreachableEvt := `
+	{
+		"uuid": "ded6289b-e557-48c1-88e1-88eb630aec21",
+		"event": "runner_on_unreachable",
+		"event_data": {
+			"play": "test",
+			"task": "Gathering Facts",
+			"host": "testhost",
+			"res": {"msg": "Failed to connect to the host via ssh"}
+		}
+	}
+	`
+
+	cases := map[string]struct {
+		events         []string
+		expectedReason string
+	}{
+		"NoEvents": {},
+		"NoFailedEvents": {
+			events: []string{playbookStartEvt},
+		},
+		"FailedEvent": {
+			events:         []string{playbookStartEvt, runnerFailedEvt},
+			expectedReason: `Failed on play "test", task "file", host "testhost": fake error`,
+		},
+		"UnreachableEvent": {
+			events:         []string{playbookStartEvt, runnerUnreachableEvt},
+			expectedReason: `Unreachable on play "test", task "Gathering Facts", host "testhost": Failed to connect to the host via ssh`,
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			dir := t.TempDir()
+			for i, evt := range tc.events {
+				if err := os.WriteFile(filepath.Join(dir, fmt.Sprintf("%d.json", i)), []byte(evt), 0600); err != nil {
+					t.Fatalf("Writing test event to file: %v", err)
+				}
+			}
+
+			reason, err := extractFailureReason(dir)
+			if err != nil {
+				t.Fatalf("Unexpected error: %v", err)
+			}
+
+			if reason != tc.expectedReason {
+				t.Errorf("Unexpected reason %v, expected %v", reason, tc.expectedReason)
 			}
 		})
 	}
